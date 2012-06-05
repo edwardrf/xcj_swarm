89e38a338d3a1678c31bad4fb3829cbf07750858
unknown <Lumi@.(none)>
2012-06-05 22:47:13 +0800

    This is my last code change

--- Blink.ino
+++ Blink.ino
@@ -1,14 +1,12 @@
 /*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
+  This is a test for Git, how to manage all those things. 
+  Quite complicated but will bite me through
  */
 
-void setup() {                
+void setup() {
   // initialize the digital pin as an output.
   // Pin 13 has an LED connected on most Arduino boards:
-  pinMode(1, OUTPUT);     
+  pinMode(1, OUTPUT);
 }
 
 void loop() {
